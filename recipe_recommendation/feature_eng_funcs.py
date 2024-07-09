95087b2271c4e8e9c309821b6cb0336c54f24afc
dashayvs <dashayovsa@gmail.com>
2024-07-10 00:56:42 +0300

    update feature_eng_funcs.py

--- recipe_recommendation/feature_eng_funcs.py
+++ recipe_recommendation/feature_eng_funcs.py
@@ -9,15 +9,50 @@ from pattern.text.en import singularize
 import re
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-CLASSIFIER = pipeline("zero-shot-classification", model="facebook/bart-large-mnli", device=device)
+CLASSIFIER = pipeline(
+    "zero-shot-classification", model="facebook/bart-large-mnli", device=device
+)
 
-COOK_METH = ['Baking', 'Boiling', 'Frying', 'Grilling', 'Steaming', 'Stewing', 'Blending', 'Raw Food',
-             'Sous Vide', 'Freezing', 'Smoking', 'Microwave']
+COOK_METH = [
+    "Baking",
+    "Boiling",
+    "Frying",
+    "Grilling",
+    "Steaming",
+    "Stewing",
+    "Blending",
+    "Raw Food",
+    "Sous Vide",
+    "Freezing",
+    "Smoking",
+    "Microwave",
+]
 
-INGREDIENTS = ["Vegetables", "Fruits", "Meat", "Seafood", "Mushrooms", "Dairy", "Grains", "Nuts"]
+INGREDIENTS = [
+    "Vegetables",
+    "Fruits",
+    "Meat",
+    "Seafood",
+    "Mushrooms",
+    "Dairy",
+    "Grains",
+    "Nuts",
+]
 
-CATEGORIES = ["Main Dish", "Appetizer", "Salad", "Dinner", "BBQ & Grilled", "Dessert", "Dressing",
-              "Sandwich", "Side Dish", "Soup", "Lunch", "Breakfast"]
+CATEGORIES = [
+    "Main Dish",
+    "Appetizer",
+    "Salad",
+    "Dinner",
+    "BBQ & Grilled",
+    "Dessert",
+    "Dressing",
+    "Sandwich",
+    "Side Dish",
+    "Soup",
+    "Lunch",
+    "Breakfast",
+]
 
 MEALS = ["Breakfast", "Snack", "Lunch", "Brunch", "Dinner", "Supper"]
 
@@ -32,10 +67,35 @@ def clean_categories0(cat_str: str) -> str:
 
 
 def clean_categories1(cat_str: str) -> str:
-    subcategories = re.split(r', | and ', cat_str)
+    subcategories = re.split(r", | and ", cat_str)
     unique_subcategories = set(subcategories)
     cat_str = ", ".join(unique_subcategories)
     return cat_str
+
+
+def generate_combinations(word1: str, word2: str, p: inflect.engine) -> List[str]:
+    singular_word1 = singularize(word1)
+    singular_word2 = singularize(word2)
+    plural_word1 = p.plural(word1)
+    plural_word2 = p.plural(word2)
+
+    combinations_list = [
+        f"{word1} {word2}",
+        f"{word2} {word1}",
+        f"{singular_word1} {singular_word2}",
+        f"{word1} {singular_word2}",
+        f"{singular_word1} {word2}",
+        f"{singular_word2} {singular_word1}",
+        f"{word2} {singular_word1}",
+        f"{singular_word2} {word1}",
+        f"{plural_word1} {plural_word2}",
+        f"{word1} {plural_word2}",
+        f"{plural_word1} {word2}",
+        f"{plural_word2} {plural_word1}",
+        f"{word2} {plural_word1}",
+        f"{plural_word2} {word1}",
+    ]
+    return combinations_list
 
 
 def singular_to_plural(cat_str: str) -> str:
@@ -54,43 +114,25 @@ def singular_to_plural(cat_str: str) -> str:
     return cat_str
 
 
-def generate_combinations(word1: str, word2: str, p: inflect.engine) -> List[str]:
-    singular_word1 = singularize(word1)
-    singular_word2 = singularize(word2)
-    plural_word1 = p.plural(word1)
-    plural_word2 = p.plural(word2)
-
-    combinations_list = [
-        f"{word1} {word2}", f"{word2} {word1}",
-        f"{singular_word1} {singular_word2}", f"{word1} {singular_word2}",
-        f"{singular_word1} {word2}", f"{singular_word2} {singular_word1}",
-        f"{word2} {singular_word1}", f"{singular_word2} {word1}",
-        f"{plural_word1} {plural_word2}", f"{word1} {plural_word2}",
-        f"{plural_word1} {word2}", f"{plural_word2} {plural_word1}",
-        f"{word2} {plural_word1}", f"{plural_word2} {word1}"
-    ]
-    return combinations_list
-
-
 def clean_categories3(cat_str: str) -> str:
     try:
         lst = cat_str.split(", ")
         p = inflect.engine()
-        found_combinations = []
+        found_combinations: List[str] = []
 
         for word1, word2 in combinations(lst, 2):
             combinations_list: List[str] = generate_combinations(word1, word2, p)
             found_combinations.extend(w for w in combinations_list if w in lst)
 
         lst = list(filter(lambda x: x not in found_combinations, lst))
-        return ', '.join(lst)
+        return ", ".join(lst)
     except Exception as e:
         print(e)
         return cat_str
 
 
 def clean_categories4(cat_str: str) -> str:
-    pattern = r'\b(?:' + '|'.join(map(re.escape, CATEGORIES)) + r')\b'
+    pattern = r"\b(?:" + "|".join(map(re.escape, CATEGORIES)) + r")\b"
     lst = cat_str.split(", ")
     cat_set = set()
     p = inflect.engine()
@@ -127,14 +169,19 @@ def get_course(cat_str: str) -> str:
 
 
 def vegan_vegetarian(cat_str: str) -> str:
-    for m in ['Vegan', 'Vegetarian']:
+    for m in ["Vegan", "Vegetarian"]:
         if m in cat_str:
             return m
     return "None"
 
 
 def convert_time_to_minutes(time_str: str) -> int:
-    formatted_str = time_str.replace(" hrs", "H").replace(" hr", "H").replace(" mins", "M").replace(" min", "M")
+    formatted_str = (
+        time_str.replace(" hrs", "H")
+        .replace(" hr", "H")
+        .replace(" mins", "M")
+        .replace(" min", "M")
+    )
     dt: datetime = datetime.strptime(formatted_str, "%HH %MM")
     return dt.hour * 60 + dt.minute
 
@@ -143,7 +190,7 @@ def get_type_cooking_batch(dir_str_list: List[str]) -> List[str]:
     batch_size = 32
     results = []
     for i in range(0, len(dir_str_list), batch_size):
-        batch = dir_str_list[i:i + batch_size]
+        batch = dir_str_list[i : i + batch_size]
         batch_results = CLASSIFIER(batch, COOK_METH)
         results.extend([res["labels"][0] for res in batch_results])
     return results
