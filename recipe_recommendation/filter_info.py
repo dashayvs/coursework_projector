f3120557aa9e5eff488628d4f2dfeb605f32ce9c
dashayvs <dashayovsa@gmail.com>
2024-08-28 20:51:36 +0300

    fix type error

--- recipe_recommendation/filter_info.py
+++ recipe_recommendation/filter_info.py
@@ -11,10 +11,10 @@ class Range:
 # todo change default values
 @dataclass
 class FilterInfo:
-    CALORIES_LIMITS: Final = Range(0, 1000)  # noqa: RUF009
-    PROTEINS_LIMITS: Final = Range(0, 40)  # noqa: RUF009
-    FATS_LIMITS: Final = Range(0, 50)  # noqa: RUF009
-    CARBS_LIMITS: Final = Range(0, 100)  # noqa: RUF009
+    CALORIES_LIMITS: Final[Range] = Range(0, 1000)  # noqa: RUF009
+    PROTEINS_LIMITS: Final[Range] = Range(0, 40)  # noqa: RUF009
+    FATS_LIMITS: Final[Range] = Range(0, 50)  # noqa: RUF009
+    CARBS_LIMITS: Final[Range] = Range(0, 100)  # noqa: RUF009
 
     methods: list[str] = field(default_factory=list)
     ingr_exclude: list[str] = field(default_factory=list)
