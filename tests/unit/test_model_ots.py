4696f83b21429c9c2c72a0572a3ec3f581aa1ecc
dashayvs <dashayovsa@gmail.com>
2024-09-04 19:01:00 +0300

    update tests for ObjectsTextSimilarity model

--- tests/unit/test_model_ots.py
+++ tests/unit/test_model_ots.py
@@ -1,8 +1,9 @@
-from unittest.mock import MagicMock
+from unittest.mock import create_autospec, patch
 
 import numpy as np
 import pandas as pd
 import pytest
+from sentence_transformers import SentenceTransformer
 
 from recipe_recommendation.dataclasses import RecipeInfo
 from recipe_recommendation.models import ObjectsTextSimilarity
@@ -18,11 +19,15 @@ DATA_EMBEDDING = np.array(
 )
 
 
+def patch_init(self):
+    self.model = create_autospec(SentenceTransformer)
+    self.duplicate_threshold = 0.98
+
+
 @pytest.fixture
-def setup_model():
-    instance = ObjectsTextSimilarity()
-    instance.model = MagicMock()
-    return instance
+def model():
+    with patch.object(ObjectsTextSimilarity, "__init__", patch_init):
+        return ObjectsTextSimilarity()
 
 
 @pytest.fixture
@@ -30,9 +35,8 @@ def query_object():
     return RecipeInfo("recipe", "ingredients")
 
 
-def test_fit(setup_model):
-    instance = setup_model
-    instance.model.encode.side_effect = [
+def test_fit(model):
+    model.model.encode.side_effect = [
         [[0.1, 0.2], [0.5, 0.6], [0.11, 0.11], [0.12, 0.12], [0.13, 0.13]],
         [[0.3, 0.4], [0.7, 0.8], [0.11, 0.11], [0.12, 0.12], [0.13, 0.13]],
     ]
@@ -49,13 +53,12 @@ def test_fit(setup_model):
             ],
         }
     )
-    instance.fit(data)
+    model.fit(data)
 
-    expected_count = 2
-    assert instance.model.encode.call_count == expected_count
+    assert model.model.encode.call_count == data.shape[1]
 
     expected_embedding = DATA_EMBEDDING
-    data_emb = instance.data_embedding
+    data_emb = model.data_embedding
     assert np.array_equal(data_emb, expected_embedding)
 
 
@@ -67,26 +70,26 @@ def test_fit(setup_model):
         (np.array([0, 1, 2, 3, 4]), 2, [0.55, 0.45, 0.8, 0.7, 0.6]),
     ],
 )
-def test_predict(setup_model, query_object, filter_ind, top_k, similarities):
-    instance = setup_model
-    instance.model.encode.side_effect = [[0.1, 0.2], [0.3, 0.6]]
-    instance.model.similarity.return_value.numpy.return_value = [np.array(similarities)]
+def test_predict(model, query_object, filter_ind, top_k, similarities):
+    model.model.encode.side_effect = [[0.1, 0.2], [0.3, 0.6]]
+    model.model.similarity.return_value.numpy.return_value = [np.array(similarities)]
 
-    instance.data_embedding = DATA_EMBEDDING
+    model.data_embedding = DATA_EMBEDDING
 
-    top_k_filter_ind = instance.predict(query_object, filter_ind=filter_ind, top_k=top_k)
+    top_k_filter_ind = model.predict(query_object, filter_ind=filter_ind, top_k=top_k)
     assert isinstance(top_k_filter_ind, np.ndarray)
     assert len(top_k_filter_ind) == top_k
     assert all(ind in filter_ind for ind in top_k_filter_ind)
 
 
-def test_save_load(setup_model, tmp_path):
-    instance = setup_model
-    instance.data_embedding = DATA_EMBEDDING
+def test_save_load(model, tmp_path):
+    model.data_embedding = DATA_EMBEDDING
     save_path = tmp_path / "model_embedding.npy"
-    instance.save(save_path)
-    loaded_instance = ObjectsTextSimilarity.load(save_path)
-    assert np.array_equal(instance.data_embedding, loaded_instance.data_embedding)
+    model.save(save_path)
+
+    with patch.object(ObjectsTextSimilarity, "__init__", patch_init):
+        loaded_instance = ObjectsTextSimilarity.load(save_path)
+    assert np.array_equal(model.data_embedding, loaded_instance.data_embedding)
 
 
 if __name__ == "__main__":
