921010748c4447b30e8649500c0a7681ba564570
dashayvs <dashayovsa@gmail.com>
2024-09-11 18:37:34 +0300

    delete old feature_eng functions

--- recipe_recommendation/feature_eng.py
+++ recipe_recommendation/feature_eng.py
@@ -61,42 +61,6 @@ VEG_TYPES: Final = ["Vegan", "Vegetarian"]
 SCORE_INGR_CLASSIFIER_THRESHOLD: Final = 0.6
 
 
-# def clean_categories0(cat_str: str) -> str:
-#     # Split the input string cat_str by commas and remove any extra spaces around elements
-#     lst = [el.strip() for el in cat_str.split(", ") if el.strip() != ""]
-#
-#     # Remove the word "Recipes" from each list element, and also filter out elements that are exactly "Recipes"
-#     lst = [el.replace(" Recipes", "") for el in lst if el != "Recipes"]
-#
-#     cat_str = ", ".join(lst)
-#     return cat_str
-
-# def clean_categories1(cat_str: str) -> str:
-#     # Split the string into subcategories by commas or the word "and".
-#     subcategories = re.split(r", | and ", cat_str)
-#     unique_subcategories = set(subcategories)
-#     cat_str = ", ".join(unique_subcategories)
-#     return cat_str
-
-# def singular_to_plural(cat_str: str) -> str:
-#     lst = cat_str.split(", ")
-#     p = inflect.engine()
-#
-#     for i, el in enumerate(lst):
-#         words = el.split(" ")
-#
-#         # Convert the last word to plural
-#         words[-1] = p.plural(words[-1])
-#
-#         # If the category consists of more than one word, join the words back into a string
-#         if len(words) > 1:
-#             lst[i] = " ".join(words)
-#
-#     cat_str = ", ".join(set(lst))
-#
-#     return cat_str
-
-
 def normalize_categories(cat_str: str) -> set[str]:
     p = inflect.engine()
 
@@ -116,52 +80,6 @@ def normalize_categories(cat_str: str) -> set[str]:
     return new_categories
 
 
-# def generate_combinations(word1: str, word2: str, p: inflect.engine) -> list[str]:
-#     singular_word1 = p.singular_noun(word1) or word1
-#     singular_word2 = p.singular_noun(word2) or word2
-#
-#     plural_word1 = p.plural(word1)
-#     plural_word2 = p.plural(word2)
-#
-#     combinations_list = [
-#         f"{word1} {word2}",
-#         f"{word2} {word1}",
-#         f"{singular_word1} {singular_word2}",
-#         f"{word1} {singular_word2}",
-#         f"{singular_word1} {word2}",
-#         f"{singular_word2} {singular_word1}",
-#         f"{word2} {singular_word1}",
-#         f"{singular_word2} {word1}",
-#         f"{plural_word1} {plural_word2}",
-#         f"{word1} {plural_word2}",
-#         f"{plural_word1} {word2}",
-#         f"{plural_word2} {plural_word1}",
-#         f"{word2} {plural_word1}",
-#         f"{plural_word2} {word1}",
-#     ]
-#     return combinations_list
-
-# def clean_categories3(cat_str: str) -> str:
-#     cat_str = " ".join(cat_str.split())
-#     lst = [cat.strip() for cat in cat_str.split(",")]
-#     p = inflect.engine()
-#     found_combinations = set()
-#
-#     # Iterate over all unique pairs of categories in the list
-#     for word1, word2 in combinations(lst, 2):
-#         # Generate possible combinations for word1 and word2 (singular/plural, etc.)
-#         combinations_list = generate_combinations(word1, word2, p)
-#
-#         # Filter the generated combinations to keep only those present in the output list
-#         valid_combinations = [w for w in combinations_list if w in lst]
-#         found_combinations.update(valid_combinations)
-#
-#     # Filter the original list to remove categories that are found in the set of combinations
-#     filtered_list = [x for x in lst if x not in found_combinations]
-#
-#     return ", ".join(filtered_list)
-
-
 def filter_out_combined_categories(categories: set[str]) -> set[str]:
     found_combinations = set()
 
@@ -176,44 +94,6 @@ def filter_out_combined_categories(categories: set[str]) -> set[str]:
             found_combinations.add(combined2)
 
     return categories - found_combinations
-
-
-# def clean_categories4(cat_str: str) -> str:
-#     # Create a regex pattern to match any category in the CATEGORIES list
-#     pattern = r"\b(?:" + "|".join(map(re.escape, CATEGORIES)) + r")\b"
-#
-#     cat_str = " ".join(cat_str.split())
-#     lst = cat_str.split(",")
-#     cat_set = set()
-#     p = inflect.engine()
-#
-#     for word in lst:
-#         # If the word is in CATEGORIES or its singular form is in CATEGORIES, add it to the set
-#         if word in CATEGORIES or p.singular_noun(word) in CATEGORIES:
-#             cat_set.add(word)
-#             continue
-#
-#         # If the word matches the regex pattern, find the match
-#         match = re.search(pattern, word)
-#
-#         if match:
-#             # If a match is found, split the word by the regex pattern
-#             parts = re.split(pattern, word, maxsplit=1)
-#
-#             # Add the first part to the set after stripping spaces
-#             cat_set.add(parts[0].strip())
-#             matched_word = match.group()
-#
-#             # If the matched word or its plural form are not already in the set, add the plural form
-#             if matched_word not in cat_set or p.plural(matched_word) not in cat_set:
-#                 cat_set.add(p.plural(matched_word))
-#         else:
-#             # If no match is found, add the word to the set
-#             cat_set.add(word)
-#
-#     cat_set.discard("")
-#
-#     return ", ".join(cat_set)
 
 
 def split_elements_by_categories(elements: set[str]) -> str:
